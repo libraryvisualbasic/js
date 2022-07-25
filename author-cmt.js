e277395c5d180cfeed527935c895d34907650c13
libraryvisualbasic <105581127+libraryvisualbasic@users.noreply.github.com>
2022-07-25 23:19:43 +0700

    Update author-cmt.js

--- author-cmt.js
+++ author-cmt.js
@@ -1,11 +1,11 @@
 $(function() {
 function highlight(){
-$(&#39;.user.blog-author&#39;).closest(&#39;.comment-block&#39;)
-.css(&#39;border&#39;, &#39;1px solid #FFA500&#39;)
-.css(&#39;background&#39;,&#39;#F1F1F2 url(&quot;https://cdn.jsdelivr.net/gh/hungvb/hungvb.com/white80.png&quot;)&#39;)
-.css(&#39;color&#39;, &#39;#444444&#39;)
-.css(&#39;font-size&#39;, &#39;18px&#39;)
-.css(&#39;padding&#39;, &#39;10px&#39;);
+$('.user.blog-author').closest('.comment-block')
+.css('border', '1px solid #FFA500')
+.css('background','#F1F1F2 url("https://cdn.jsdelivr.net/gh/hungvb/hungvb.com/white80.png")')
+.css('color', '#444444')
+.css('font-size', '18px')
+.css('padding', '10px');
 }
-$(document).bind(&#39;ready scroll click&#39;, highlight);
+$(document).bind('ready scroll click', highlight);
 });
